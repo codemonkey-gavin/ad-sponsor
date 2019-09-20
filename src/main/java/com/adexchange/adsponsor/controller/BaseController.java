036c2ef2f0f1befe853f7b22143ca04bec29ef03
Gavin Cheng <chenggang_cn@126.com>
2019-09-20 10:37:43 +0800

    add advertiser changemaker

--- src/main/java/com/adexchange/adsponsor/controller/BaseController.java
+++ src/main/java/com/adexchange/adsponsor/controller/BaseController.java
@@ -9,12 +9,12 @@ import javax.servlet.http.HttpSession;
 public class BaseController {
     protected HttpServletRequest request;
     protected HttpServletResponse response;
-    protected HttpSession session;
+    //protected HttpSession session;
 
     @ModelAttribute
     public void init(HttpServletRequest request, HttpServletResponse response){
         this.request = request;
         this.response = response;
-        this.session = request.getSession();
+        //this.session = request.getSession();
     }
 }
